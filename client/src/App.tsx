5ecb12ee37a94e2e304011192bc7542f95d4ea49
Alamin <alaminshaheen@iut-dhaka.edu>
2021-09-11 22:08:24 +0600

    initialized routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import NavigationBar from "Components/GenericComponents/NavigationBar";
 import { Home } from "Pages/Home";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 const App = () => {
 	// return <Home />;
 	return (
 		<>
 			<NavigationBar />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/">
+						{userState.Id ? <Home /> : <Redirect to="/login" />}
+					</Route>
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+				</Switch>
+			</BrowserRouter>
 			<Home />
 		</>
 	);
